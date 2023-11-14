69a02be1e84d30bad7661d7f312ab631c8fc3f32
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-11-14 23:46:28 +0100

    creados calendario y meeting form, y de mas

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -10,31 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
         method : "GET"
     }).then(response => response.json())
     .then(data => {
-        if (Array.isArray(data)) {
-            // Loop through each item in the array
-            data.forEach(item => {
-              // Do something with each item
-                console.log(item);
-                console.log("array")
-            
-                const listItem = createStudentListItem(item.name, item.age);
-                studentList.appendChild(listItem);
-                studentForm.reset();
 
-            });
-          } else if (typeof data === 'object') {
-            // Loop through each key in the object
-            Object.keys(data).forEach(key => {
-              // Access the value of each key
-                console.log(`${key}: ${data[key]}`);
-                console.log("object")
-                const listItem = createStudentListItem(data[key].name, data[key].age);
-                studentList.appendChild(listItem);
-                studentForm.reset();
-            });
-          } else {
-            console.error('Invalid data format');
-          }
+        // Loop through each item in the array
+        data.forEach(item => {
+            // Do something with each item
+            console.log(item);
+            console.log("array")
+        
+            const listItem = createStudentListItem(item.name, item.age, item.id);
+            studentList.appendChild(listItem);
+            studentForm.reset();
+
+        });
     })
 
 
@@ -225,5 +212,170 @@ document.addEventListener('DOMContentLoaded', () => {
     showPresentacionView();
 
 
+    
+
 
 });
+
+const reunionesElement = document.getElementById("reuniones");
+const cabeceraElement = document.getElementById("cabecera");
+const mesElement = document.getElementById("mes");
+const submitMettingButton = document.getElementById("meetingSubmitButton");
+const meetingList = document.getElementById("meeting-list");
+
+let fechaActual = new Date();
+//fechaActual=new Date(2022,11,24);
+const idioma = "es";
+const intlMes = new Intl.DateTimeFormat(idioma, {month:'long'});
+const intlSemana = new Intl.DateTimeFormat(idioma,{weekday:'short'});
+
+const diaSemana = [...Array(7).keys()].map((i)=>intlSemana.format(new Date(2023,4,i+1)));
+
+
+
+function asignaCabecera(año, indiceMes){
+    let nombreMes = intlMes.format(new Date(año,indiceMes)).toUpperCase();
+    cabeceraElement.textContent=`${nombreMes} ${año}`;
+}
+
+function asignaDiaMes(año,indiceMes){
+    const diasEnMes = new Date(año,indiceMes+1,0).getDate();
+    const dias = [...Array(diasEnMes).keys()];
+    let primerDia = new Date(año,indiceMes,1).getDay();
+    if(primerDia===0) primerDia = 7;
+
+    const claseCSSPrimerDia = `class='primerDia dia' style='--primerDiaMes:${primerDia}'`;
+    const htmlDiaSemana = diaSemana.map((nombre)=>`<li class='diaNombre'>${nombre}</li>`).join('');
+    const cal = dias.map((dia,indice)=>`<li ${indice === 0 ? claseCSSPrimerDia:"class='dia'"}>${dia+1}</li>`).join('');
+    mesElement.innerHTML=htmlDiaSemana+" "+cal;
+}
+
+function mostarReunionesDia(event){
+    meetingList.innerHTML = '';
+    const dia = event.target.textContent;
+    // reunionesElement.textContent=`No hay reuniones en el dia ${dia}`;
+
+    const meetingDate = `2023-11-${dia}`;
+    fetch(`http://localhost:3000/meetings?meetingDate=${meetingDate}`, {
+        method : "GET"
+    }).then(response => response.json())
+    .then(data => {
+            
+        data.forEach(item => {
+            const listItem = createMeetingListItem(item.meetingName, item.startHour, item.endHour, item.id);
+            meetingList.appendChild(listItem);
+            // studentForm.reset();
+        });
+
+    });
+
+}
+
+
+
+asignaCabecera(fechaActual.getFullYear(),fechaActual.getMonth());
+asignaDiaMes(fechaActual.getFullYear(),fechaActual.getMonth());
+
+document.querySelectorAll('.dia').forEach(dia=>dia.addEventListener('click',mostarReunionesDia));
+
+
+function createMeetingListItem(name, startHour, endHour, id) {
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+
+    listItem.setAttribute('id', id);
+    console.log(`nada mas tal: ${listItem.getAttribute('id')}`);
+
+    const studentInfo = document.createElement("span");
+    studentInfo.textContent = `${name}  ${startHour} - ${endHour}`;
+
+    const buttonsDiv = document.createElement("div");
+
+    const updateButton = document.createElement("button");
+    // updateButton.textContent = "Update";
+    const updateIcon = document.createElement("i");
+    updateIcon.className = "bi bi-pencil-square"; // Bootstrap trash icon
+    updateButton.appendChild(updateIcon);
+    updateButton.className = "btn btn-success mr-2";
+
+    const removeButton = document.createElement("button");
+    const removeIcon = document.createElement("i");
+    removeIcon.className = "bi bi-trash"; // Bootstrap trash icon
+    removeButton.appendChild(removeIcon);
+    removeButton.className = "btn btn-danger";
+
+    // removeButton.className = "btn btn-danger";
+
+    updateButton.addEventListener("click", () => {
+        alert(`Update Student: Name - ${name}, Age - ${age}`);
+    });
+
+    removeButton.addEventListener("click", () => {
+        const id = listItem.getAttribute('id');
+        listItem.remove();
+        fetch(`http://localhost:3000/meetings/${id}`, {
+            method : "DELETE"
+        })
+        .then(response => response.json())
+        .then(datos => console.log(datos));
+    });
+
+    buttonsDiv.appendChild(updateButton);
+    buttonsDiv.appendChild(removeButton);
+
+    listItem.appendChild(studentInfo);
+    listItem.appendChild(buttonsDiv);
+
+
+    return listItem;
+}
+
+
+
+submitMettingButton.addEventListener("click", function (e) {
+
+    e.preventDefault();
+    const startHour = document.getElementById("startHour").value;
+    const endHour = document.getElementById("endHour").value;
+    const interval = document.getElementById("interval").value;
+    const studentsPerInterval = document.getElementById("studentsPerInterval").value;
+    const meetingDate = document.getElementById("meetingDate").value;
+    const groupSelector = document.getElementById("groupSelector");
+    const subject = document.getElementById("subject").value;
+    const teacher = document.getElementById("teacher").value;
+    const location = document.getElementById("location").value;
+    const meetingName = document.getElementById("meetingName").value;
+
+    const data = {
+        startHour,
+        endHour,
+        interval,
+        studentsPerInterval,
+        meetingDate,
+        group : groupSelector.value,
+        subject,
+        teacher,
+        location,
+        meetingName
+    }
+
+    fetch("http://localhost:3000/meetings/", {
+        method : "POST",
+        body : JSON.stringify(data),
+        headers : {
+            "Content-type" : "application/json"
+        },
+    })
+    .then(response => response.json())
+    .then(datos => {
+        console.log(datos)
+        // const listItem = createMeetingListItem(meetingName, startHour, endHour, datos.id);
+        // meetingList.appendChild(listItem);
+        // // meetingForm.reset();
+    });
+
+});
+
+
+
+
